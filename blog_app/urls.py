c10450f960f6600f8b0259cff5492dee483ff02a
Whasnat <w.hasnat94@gmail.com>
2020-08-21 01:17:27 -0700

    individual Blog Detail added

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'blog_app'
+
 urlpatterns = [
     path('', views.blog_home, name = 'blog_home' ),
+    path('<int:blog_id>', views.blog_details, name = 'blog_details'),
 ]
 
 
